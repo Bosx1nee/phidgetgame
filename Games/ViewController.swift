0a07a305febaef3f1b17ef6f9cd1986bdf774a42
Your Name <you@example.com>
2018-11-28 14:34:54 -0700

    Initial Commit

---
+++ Games/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Games
+//
+//  Created by Dionn Felipe on 2018-11-28.
+//  Copyright © 2018 Dionn Felipe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
